c6ce96d286d1f18cb72c91485fb981fc8011cf1b
tijumathew <tiju.george18@gmail.com>
2016-12-05 00:13:43 -0500

    sum of array

---
+++ sum-of-array.py
@@ -0,0 +1,15 @@
+import sys
+import os 
+def sum(numbers):
+    ret=0
+    for i in numbers:
+        ret += i
+    return ret
+    
+print sum([1,2,3,4,5])
+
+
+
+
+#f= open(os.environ['OUTPUT_PATH'],'w')
+
