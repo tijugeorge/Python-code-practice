b4a480696d2065903f6a5465dc02e03f7046a7c6
tijumathew <tiju.george18@gmail.com>
2017-01-02 11:55:02 -0500

    Add files via upload

---
+++ GuessingGame.py
@@ -0,0 +1,12 @@
+import random
+
+highest = 10
+answer = random.randrange(highest)
+guess = raw_input("Guess a number from 0 to %d:" %highest)
+while (int(guess)!=answer):
+    if (int(guess)<answer):
+        print "Answer is higher"
+    else:
+        print "Answer is lower"
+    guess = raw_input("Guess a number from 0 to %d: "%highest)
+raw_input("You're a winner Face!!!")
