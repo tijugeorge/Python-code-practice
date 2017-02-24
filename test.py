d05d3ff74cb884c76858f1378d447f2c1804a3c5
tijumathew <tiju.george18@gmail.com>
2017-02-23 23:36:24 -0500

    testing elasticsearch

---
+++ test.py
@@ -0,0 +1,11 @@
+import requests
+import json
+#r = requests.get('http://localhost:9200')
+r = requests.get('http://swapi.co/api/people/')
+i = 1
+while r.status_code == 200:
+    r = requests.get('http://swapi.co/api/people/'+ str(i))
+    es.index(index='sw', doc_type='people', id=i, body=json.loads(r.content))
+    i=i+1
+
+print(i)
