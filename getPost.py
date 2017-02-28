1d9a3686642a2b22000a12f0224c83a1b0c2099c
tijumathew <tiju.george18@gmail.com>
2017-02-27 23:01:09 -0500

    learning google api use

---
+++ getPost.py
@@ -0,0 +1,18 @@
+import requests
+
+URL = "http://maps.googleapis.com/maps/api/geocode/json"
+
+location = 'New York University'
+
+PARAMS = {'address': location}
+
+r = requests.get(url = URL, params = PARAMS)
+
+data = r.json()
+
+latitude = data['results'][0]['geometry']['location']['lat']
+longitude = data['results'][0]['geometry']['location']['lng']
+formatted_address = data['results'][0]['formatted_address']
+
+print("Latitude:%s\nLongitude:%s\nFormatted Address:%s"
+      %(latitude, longitude,formatted_address))
