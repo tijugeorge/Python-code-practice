ad6848f664add98f1a0c5dc01579cdef5fd3e313
tijumathew <tiju.george18@gmail.com>
2017-02-28 22:35:20 -0500

    del node from LL

---
+++ NodeDel.py
@@ -0,0 +1,14 @@
+class LinkedListNode:
+
+    def __init__(self, value):
+        self.value = value
+        self.next  = None
+
+a = LinkedListNode('A')
+b = LinkedListNode('B')
+c = LinkedListNode('C')
+
+a.next = b
+b.next = c
+
+#delete_node(b)
