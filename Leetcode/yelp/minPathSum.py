d3fe4408817cf1f4fc09df782ef869ada89013fd
Tiju George <tiju.m.george@gmail.com>
2018-01-12 21:53:17 -0500

    Create minPathSum.py

---
+++ Leetcode/yelp/minPathSum.py
@@ -0,0 +1,21 @@
+
+def minPathSum(grid):
+  m = len(grid)
+  n = len(grid[0])
+  for i in range(1, m):
+    grid[i][0] += grid[i-1][0]
+  for i in range(1, n):
+    grid[0][i] += grid[0][i-1]
+  for i in range(1, n):
+    for j in range(1, m):
+      grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+  return grid[-1][-1]
+  
+  
+  
+grid = [[1,3,1],
+ [1,5,1],
+ [4,2,1]]
+ 
+ 
+print(minPathSum(grid))
